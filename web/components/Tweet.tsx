bf783c3a1189c6b4570742ef5d67787d9877cd7d
Saulo Candeira <61151496+SauloCandeira@users.noreply.github.com>
2022-12-13 17:35:32 -0300

    Add files via upload

---
+++ web/components/Tweet.tsx
@@ -0,0 +1,9 @@
+export function Tweeet(props) {
+    return(
+        <div>
+            <h1> Tweet</h1>
+            <p> {props.text} </p>
+            <button> Curir </button>
+        </div>
+    )
+}
